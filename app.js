fd8139cb5fc753d03fafe555fec54fef2047c3db
Dylan Taft <dylbronjames@gmail.com>
2019-10-01 22:23:15 -0600

    added success and failure pages

--- app.js
+++ app.js
@@ -15,7 +15,48 @@ app.post("/", function (req, res) {
   var firstName = req.body.fName;
   var lastName = req.body.lName;
   var email = req.body.email;
-  console.log(firstName, lastName, email);
+
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+
+  jsonData = JSON.stringify(data);
+
+  var options = {
+    url: "https://us20.api.mailchimp.com/3.0/lists/69faa91ebb",
+    method: 'POST',
+    headers: {
+      "Authorization": "dylan1 16c239a23e4e56615ba6172c4f250bad-us20"
+    },
+    body: jsonData
+  };
+
+  request(options, function (error, response, body) {
+    if (error) {
+      console.log(error);
+      res.sendFile(__dirname + "/failure.html");
+    } else {
+      console.log(response.statusCode);
+      if (response.statusCode === 200) {
+        res.sendFile(__dirname + "/success.html");
+      }
+    }
+  });
+
+});
+
+app.post("/failure", function (req, res) {
+  res.redirect("/");
 })
 
 app.listen(3000, function () {
